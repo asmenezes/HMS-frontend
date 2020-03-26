96dd845d39753f9c0e49df21f350847a292109dd
asmenezes <asmenezes23@gmail.com>
2020-03-26 16:58:55 -0400

    Created Variables file

---
+++ src/app/variables.ts
@@ -0,0 +1,11 @@
+import { RoomSearchItem } from './models/roomsearchitem';
+import { RoomResult } from './models/roomresult';
+
+
+
+export let searchedItem:RoomSearchItem = [];
+
+
+// roomresult item with room nums, room descriptions and stuff
+
+// Account stuff
